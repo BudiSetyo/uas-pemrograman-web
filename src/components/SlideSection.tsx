75aec2a02e34aa9ed25a080bbaf5f1de64658f57
walsptr <syawal.als.com@gmail.com>
2025-07-19 18:50:44 +0700

    update images slider

--- src/components/SlideSection.tsx
+++ src/components/SlideSection.tsx
@@ -43,7 +43,7 @@ export function SlideSection() {
                         alt="..." />
                     </div>
                     <div className="hidden duration-700 ease-in-out" data-carousel-item>
-                        <img src="https://github.com/BudiSetyo/uas-pemrograman-web/blob/master/public/slide-4.jpeg?raw=true" 
+                        <img src="https://github.com/BudiSetyo/uas-pemrograman-web/blob/master/public/slide-4.jpg?raw=true" 
                         className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
                         alt="..." />
                     </div>
